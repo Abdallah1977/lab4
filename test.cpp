c91a47d51392b88bd23693f0138cb6e14cf12822
Abdallah1977 <abdallahmoh197@gmail.com>
2025-12-12 20:42:52 +0200

    c++

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+
+    cout << "Hello from C++!" << endl;
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+}
